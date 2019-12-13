42245129677dbce6675cf32c41f9798c63c0c74c
Aloys Berger <aloysberger@gmail.com>
2019-12-13 12:27:32 +0100

    Focus on mobile first design

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //Sidebar toggle button 
 const sidebarButton = document.querySelector('#toggleNavbar');
 const sidebar = document.querySelector('nav');
+let isCollapsed = false;
+const sidebarHeight = sidebar.offsetHeight;
+const buttonHeight = sidebarButton.offsetHeight;
 
+sidebarButton.style.top = `${sidebarHeight}px`;
 sidebarButton.addEventListener('click', toggleMenu);
 
 function toggleMenu () {
   sidebar.classList.toggle('collapse');
+  (!isCollapsed) ?
+    sidebarButton.style.transform = `translateY(-${sidebarHeight - buttonHeight}px`:
+    sidebarButton.style.transform = `translateY(0px)`;
+  isCollapsed = !isCollapsed;
 }
 
 
